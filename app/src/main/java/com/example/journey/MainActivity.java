b6bbc570bb810ef071ad212dd1a9a464bf4b3756
APOLLON <iratus7@yahoo.gr>
2022-03-31 23:11:37 +0300

    Database Update

--- app/src/main/java/com/example/journey/MainActivity.java
+++ app/src/main/java/com/example/journey/MainActivity.java
@@ -1,17 +1,22 @@
 package com.example.journey;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.room.Room;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    public static JourneyDatabase myAppDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        JourneyRepository journeyRepository =new JourneyRepository(getApplicationContext());
+        //JourneyRepository journeyRepository =new JourneyRepository(getApplicationContext());
+        myAppDatabase = Room.databaseBuilder(getApplicationContext(),JourneyDatabase.class,"journeydb").allowMainThreadQueries().build();
+
+
 
 
     }
