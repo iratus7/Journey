8ee8f64abab90c47d68bbb5b40a5596c0cb4453e
APOLLON <iratus7@yahoo.gr>
2022-04-05 04:20:02 +0300

    ΕΒΑΛΑ ΚΑΙ ΤΟ DRAWER MENU

--- app/src/main/java/com/example/journey/MainActivity.java
+++ app/src/main/java/com/example/journey/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.journey;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.room.Room;
 
 import android.os.Bundle;
@@ -9,16 +13,34 @@ public class MainActivity extends AppCompatActivity {
     public static JourneyDatabase journeyDatabase;
     private String DB_NAME = "journeydb";
 
+    private DrawerLayout drawer;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawer=findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawer,toolbar,R.string.navigation_drawer_open,R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
         //JourneyRepository journeyRepository =new JourneyRepository(getApplicationContext());
         journeyDatabase = Room.databaseBuilder(getApplicationContext(),JourneyDatabase.class,DB_NAME).allowMainThreadQueries().build();
 
-        TravelAgency travelAgency1 = new TravelAgency(2,"hermes","dragoymi");
-        MainActivity.journeyDatabase.journeyDao().insertTravelAgency(travelAgency1);
+        //TravelAgency travelAgency1 = new TravelAgency(2,"hermes","dragoymi");
+        //MainActivity.journeyDatabase.journeyDao().insertTravelAgency(travelAgency1);
 
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        }else {
+            super.onBackPressed();
+        }
     }
 }
