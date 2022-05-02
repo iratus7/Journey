baf11250ed6c508a4be8fa1a36e0cd66c90e1ffb
APOLLON <iratus7@yahoo.gr>
2022-05-02 07:37:23 +0300

    add about menu with specs of the phone

--- app/src/main/java/com/example/journey/MainActivity.java
+++ app/src/main/java/com/example/journey/MainActivity.java
@@ -7,10 +7,20 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.room.Room;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     public static FirebaseFirestore db;
@@ -35,11 +45,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         drawer.addDrawerListener(toggle);
         toggle.syncState();
 
-        //JourneyRepository journeyRepository =new JourneyRepository(getApplicationContext());
         journeyDatabase = Room.databaseBuilder(getApplicationContext(),JourneyDatabase.class,DB_NAME).allowMainThreadQueries().build();
-
-        //TravelAgency travelAgency1 = new TravelAgency(3,"hermes","dragoymi");
-        //MainActivity.journeyDatabase.journeyDao().insertTravelAgency(travelAgency1);
 
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainerAction, new HomeFragment()).commit();
@@ -75,8 +81,6 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                 getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainerAction, new SearchFragmentActions()).commit();
                 getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainerResults, new SearchFragmentResults()).commit();
                 break;
-
-
         }
         drawer.closeDrawer(GravityCompat.START);
         return true;
@@ -89,5 +93,46 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         }else {
             super.onBackPressed();
         }
+    }
+
+
+    //about menu
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_settings) {
+
+            try {
+                Process process = Runtime.getRuntime().exec("cat /proc/cpuinfo");
+                BufferedReader in = new BufferedReader(new InputStreamReader(process.getInputStream()));
+                String cpuInfo=in.readLine();
+                process = Runtime.getRuntime().exec("cat /proc/meminfo");
+                in = new BufferedReader(new InputStreamReader(process.getInputStream()));
+                Integer mem=Integer.parseInt(in.readLine().replaceAll("\\D+",""));
+                Double memGb= (double)mem /1048576;
+
+                AlertDialog alertDialog = new AlertDialog.Builder(MainActivity.this).create(); //Read Update
+                alertDialog.setTitle("ABOUT");
+                alertDialog.setMessage("JOURNEY App 1.0" + "\n" + "Model: " + Build.MANUFACTURER + " - " + Build.MODEL + "\n" + "Android " + Build.VERSION.RELEASE + " (API " + Build.VERSION.SDK_INT + ")" + "\n" + "CPU" + cpuInfo.substring(11) + "\n" +"MEMORY: "+String.format("%.2f", memGb) +"Gb");
+
+                alertDialog.setButton("ΣΥΝΕΧΕΙΑ", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        // here you can add functions
+                    }
+                });
+                alertDialog.show();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
